17c3f906b2fdcacf328e21142b02ef7dff2157dc
Sasa Miljatovic <sasasasamijatmijat@gmail.comSale@DESKTOP-9RT02VE MINGW64 ~$ touch index.htmlSale@DESKTOP-9RT02VE MINGW64 ~$ touch app.jsSale@DESKTOP-9RT02VE MINGW64 ~$ git initInitialized empty Git repository in C:/Users/Sale/.git/Sale@DESKTOP-9RT02VE MINGW64 ~ (master)$ git config --global user.name Sasa>
2022-05-06 15:43:03 +0200

    AboutUs

---
+++ Brodovi projekat/BoatsMontenegro/Controllers/AboutUsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoatsMontenegro.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        // GET: AboutUs
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
