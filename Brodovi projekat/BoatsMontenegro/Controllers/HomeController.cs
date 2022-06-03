aa9321a63c43aa1bdea22cdd869d6448a737102e
Sasa Miljatovic <sasasasamijatmijat@gmail.comSale@DESKTOP-9RT02VE MINGW64 ~$ touch index.htmlSale@DESKTOP-9RT02VE MINGW64 ~$ touch app.jsSale@DESKTOP-9RT02VE MINGW64 ~$ git initInitialized empty Git repository in C:/Users/Sale/.git/Sale@DESKTOP-9RT02VE MINGW64 ~ (master)$ git config --global user.name Sasa>
2022-06-03 16:51:05 +0200

    Index1 and Foms1

--- Brodovi projekat/BoatsMontenegro/Controllers/HomeController.cs
+++ Brodovi projekat/BoatsMontenegro/Controllers/HomeController.cs
@@ -17,7 +17,32 @@ namespace BoatsMontenegro.Controllers
         {
             objContext = new BaseContext();
         }
-        
+        //public ViewResult Pretraga (string sortOrder, string searchString)
+        //{
+        //    ViewBag.CategorySortParam = String.IsNullOrEmpty(sortOrder) ? "category_desc" : "";
+        //    ViewBag.PriceSortParam = sortOrder == "Price" ? "price_desc" : "Price";
+        //    var boatsList = from s in objContext.Boats select s;
+        //    if (!String.IsNullOrEmpty(searchString))
+        //    {
+        //        boatsList = boatsList.Where(s => s.Category.Contains(searchString) || s.Price.ToString().Contains(searchString));
+        //    }
+        //    switch (sortOrder)
+        //    {
+        //        case "category_desc":
+        //            boatsList = boatsList.OrderByDescending(s => s.Category);
+        //            break;
+        //        case "Price":
+        //            boatsList = boatsList.OrderBy(s => s.Price);
+        //            break;
+        //        case "price_desc":
+        //            boatsList = boatsList.OrderByDescending(s => s.Price);
+        //            break;
+        //        default:
+        //            boatsList = boatsList.OrderBy(s => s.Category);
+        //            break;
+        //    }
+        //    return View(boatsList.ToList());
+        //}
         
         public ActionResult Index(string search, string SortOrder, string SortBy)
         {
@@ -26,8 +51,9 @@ namespace BoatsMontenegro.Controllers
             if (search != null)
             {
                 //boats = boats.Where(x => x.Size.Contains(search) || x.Category.Contains(search)).ToList();
-                return View(objContext.Boats.Where(x => x.Category.Contains(search) && x.Size.Contains(search)
-                && x.Capacity.ToString().Contains(search) && x.Price.ToString().Contains(search)).ToList());
+                return View(objContext.Boats.Where(x => x.Category.Contains(search)).ToList());
+                //&& x.Size.Contains(search)
+                //&& x.Capacity.ToString().Contains(search) && x.Price.ToString().Contains(search)).ToList());
             }
 
             ViewBag.sortorder = SortOrder;
